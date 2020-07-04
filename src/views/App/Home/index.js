02c305d9eeebc4835090bf36471f54d50f6ad019
Vinicius <vinicius@iMac-de-Vinicius.local>
2020-07-04 20:42:48 -0300

    [Esqueleto] Implementação da lista horizontal de meses

--- src/views/App/Home/index.js
+++ src/views/App/Home/index.js
@@ -1,10 +1,72 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import {
+  View,
+  Text,
+  Dimensions,
+  ScrollView,
+  TouchableOpacity,
+} from 'react-native';
+
+import { months } from '../../../mocks/months';
+
+const screenWidth = Math.round(Dimensions.get('window').width);
+const thirdW = screenWidth / 3;
 
 const Home = () => {
+  const today = new Date();
+  const MonthRef = useRef();
+
+  const [selectedMounth, setSelectedMounth] = useState(today.getMonth());
+
+  const handleScrollEnd = (e) => {
+    const posX = e.nativeEvent.contentOffset.x;
+    const targetMonth = Math.round(posX / thirdW);
+    setSelectedMounth(targetMonth);
+  };
+
+  const scrollToMonth = (m) => {
+    const posX = m * thirdW;
+    MonthRef.current.scrollTo({ x: posX, y: 0, animated: true });
+  };
+
+  useEffect(() => {
+    setTimeout(() => {
+      scrollToMonth(selectedMounth);
+    }, 1);
+  }, [selectedMounth]);
+
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={{ flex: 1 }}>
+      <View style={{}}>
+        <ScrollView
+          horizontal
+          ref={MonthRef}
+          showsHorizontalScrollIndicator={false}
+          decelerationRate="fast"
+          snapToInterval={thirdW}
+          contentContainerStyle={{ paddingLeft: thirdW, paddingRight: thirdW }}
+          onMomentumScrollEnd={handleScrollEnd}
+          style={{ height: 60 }}
+        >
+          {months.map((m, k) => (
+            <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+              <TouchableOpacity
+                key={k}
+                style={{
+                  width: thirdW,
+                  alignItems: 'center',
+                }}
+                onPress={() => setSelectedMounth(k)}
+              >
+                <Text style={{ color: k === selectedMounth ? '#00f' : '#000' }}>
+                  {m}
+                </Text>
+              </TouchableOpacity>
+            </View>
+          ))}
+        </ScrollView>
+        <Text>{selectedMounth}</Text>
+      </View>
     </View>
   );
 };
